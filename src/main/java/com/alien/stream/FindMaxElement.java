1d71e1041abbef9211859457dc8206bdc45db13c
Chetan Malabade <malabade.chetan@gmail.com>
2024-06-26 21:00:10 +0530

    max elemetnt

---
+++ src/main/java/com/alien/stream/FindMaxElement.java
@@ -0,0 +1,20 @@
+package com.alien.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+//Given a list of integers, find the maximum value element present in it using Stream functions?
+
+public class FindMaxElement {
+
+	public static void main(String[] args) {
+
+		List<Integer> myList = Arrays.asList(10, 15, 8, 49, 25, 98, 98, 32, 15);
+
+		Integer max = myList.stream().max(Integer::compare).get();
+
+		System.out.println(max);
+
+	}
+
+}
---
+++ src/main/java/com/alien/stream/FindSecondMaxElement.java
@@ -0,0 +1,24 @@
+package com.alien.stream;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class FindSecondMaxElement {
+
+	public static void main(String[] args) {
+
+		List<Integer> myList = Arrays.asList(10, 15, 8, 49, 25, 98, 98, 32, 15);
+
+		Integer secondMax = myList.stream()
+								.distinct()
+								.sorted(Comparator.reverseOrder())
+								.skip(1)
+								.findFirst()
+								.get();
+		
+		System.out.println(secondMax);
+
+	}
+
+}
