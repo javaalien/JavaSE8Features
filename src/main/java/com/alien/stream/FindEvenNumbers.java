5e1e461a0e3542975e651cc590935f8be7238f63
Chetan Malabade <malabade.chetan@gmail.com>
2024-06-26 20:46:17 +0530

    stream programs

--- src/main/java/com/alien/stream/FindEvenNumbers.java
+++ src/main/java/com/alien/stream/FindEvenNumbers.java
@@ -4,13 +4,19 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
+//Given a list of integers, find out all the even numbers that exist in the list using Stream functions?
+
 public class FindEvenNumbers {
 
 	public static void main(String[] args) {
 		
 		List<Integer> list = Arrays.asList(10,15,8,49,25,98,32);
 		
-		list.stream().filter(i->i%2==0).collect(Collectors.toList()).forEach(System.out::println);
+		list.stream()
+			.filter(i->i%2==0)
+			.collect(Collectors.toList())
+			.forEach(System.out::println);
 		
 	}
 
---
+++ src/main/java/com/alien/stream/NumberStartingWithOne.java
@@ -0,0 +1,18 @@
+package com.alien.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+//Given a list of integers, find out all the numbers starting with 1 using Stream functions?
+
+public class NumberStartingWithOne {
+
+	public static void main(String[] args) {
+		List<Integer> myList = Arrays.asList(10, 15, 8, 49, 25, 98, 32);
+
+		myList.stream()
+				.map(s -> s + "")
+				.filter(s -> s.startsWith("1"))
+				.forEach(System.out::println);
+	}
+}
